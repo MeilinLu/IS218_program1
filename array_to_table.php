cca29d06a744bb7a2d6cbe0e7883ce38839a6288
meilinlu86@gmail.com <ml473@afsconnect1.njit.edu>
2016-10-25 12:55:01 -0400

    add a file array to a html table

---
+++ array_to_table.php
@@ -0,0 +1,77 @@
+<html>
+  <head>
+    <title>Turning an array into a table</title>
+    <style>
+      table {
+        border-collapse: collapse;
+	}
+	td, th {
+	  border: 1px solid #999;
+	    padding: 0.5rem;
+	      text-align: left;
+	      }
+    </style>
+  </head>
+  <body>
+  <?php
+$myarray   = array();
+$myarray[] = array(
+    "Name" => "Jim",
+    "ID" => "00001",
+    "Favorite Color" => "Blue"
+);
+$myarray[] = array(
+    "Name" => "Sue",
+    "ID" => "00002",
+    "Favorite Color" => "Red"
+);
+$myarray[] = array(
+    "Name" => "Barb",
+    "ID" => "00003",
+    "Favorite Color" => "Green"
+);
+class html
+{
+    public $html;
+    public function __construct($html = array())
+    {
+        // echo $html Array;
+    }
+}
+class htmlTable extends html
+{
+    protected $htmlTable;
+    public function getHTML($html)
+    {
+        $this->html = $html;
+    }
+    public function getTableHTML()
+    {
+        $this->htmlTable .= '<table>';
+        $this->htmlTable .= '<thead>';
+        $this->htmlTable .= '<tr>';
+        //Table Head
+        foreach ($this->html[0] as $key => $value) {
+            $this->htmlTable .= '<th>' . $key . '</th>';
+        }
+        $this->htmlTable .= '</tr>';
+        $this->htmlTable .= '</thead>';
+        //Table Body
+        $this->htmlTable .= '<tbody>';
+        foreach ($this->html as $row) {
+            $this->htmlTable .= '<tr>';
+            foreach ($row as $key => $value) {
+                $this->htmlTable .= '<td>' . $value . '</td>';
+            }
+            $this->htmlTable .= '</tr>';
+        }
+        $this->htmlTable .= '</tbody>';
+        echo $this->htmlTable;
+    }
+}
+$obj = new htmlTable;
+$obj->getHTML($myarray);
+$obj->getTableHTML();
+?>
+  </body>
+</html>
--- factoryPatten.php
+++ factoryPatten.php
@@ -21,7 +21,7 @@ class carFactory{
 //version1&2: $car = carFactory::createCar('ford','fiesta','2016');
 
 $cars[] = carFactory::createCar('ford', 'fiesta', '2016');
-print_r($cars);
+
 
 //version2:$collection[] = $car;
 
@@ -32,4 +32,30 @@ print_r($cars);
 //version1: print a single car info
 //version2: print an array which may contains many cars' info
 //version3: simplify the version2
+
+
+function readCSV($csvFile){
+    $file_handle = fopen($csvFile, 'r');
+    // make variable -> use make,model,year as the keys 
+    $line_count = 0;
+    while (!feof($file_handle) ) {
+        //$line_of_text[] = fgetcsv($file_handle, 1024);
+    	
+	//use the make, model, year as the keys
+        if($line_count ==0){
+	$field_names = array_values(fgetcsv($file_handle, 1024));
+	//print_r($field_names);
+        
+        }
+        $records = array_combine($field_names, fgetcsv($file_handle,1024));
+        print_r($records);
+    }
+    fclose($file_handle);
+    return $line_of_text;
+}
+
+    $cars = readCSV('cars.csv');
+    print_r($cars);
+
+
 ?>
