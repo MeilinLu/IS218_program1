f9438d564224aa93c399c56f782774bc09839a10
meilinlu86@gmail.com <ml473@afsconnect1.njit.edu>
2016-10-26 17:03:46 -0400

    string practice 1-5

--- stringPractice.php
+++ stringPractice.php
@@ -1,11 +1,11 @@
 <?php
-  echo '#1: string addslashes(string $str)';
+  echo "\n".'#1: string addslashes(string $str)'."\n";
   $str = "Is your name O'Reilly?";
   echo "$str";
   echo addslashes($str);
 ?>
 <?php 
-  echo '#2: string chr (int $ascii)';
+  echo "\n".'#2: string chr (int $ascii)'."\n";
   echo chr(-159),chr(833);
   $str = "The string ends in excape: ";
   $str .= chr(27);
@@ -13,7 +13,19 @@
 
 ?>
 <?php
-  echo '#3 chunk_split';
+  echo "\n".'#3 chunk_split'."\n";
   $str = "0123456789";
   echo substr(chunk_split($str,1,':'), 0, 19);
+?>
+<?php
+  echo "\n".'#4 count_chars()'."\n";
+  $data = "Two Ts and one F.";
+  foreach(count_chars($data,1) as $i=>$val){
+    echo 'There were '.$val.' instance of "'.chr($i).'" in the string.'."\n";
+  }
+?>
+<?php
+  echo "\n".'#5 echo '."\n";
+  echo "Sum: ", 1+2;
+  echo "Hello", 
 ?>
