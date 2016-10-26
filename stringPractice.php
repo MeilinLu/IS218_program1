8339e77c1bcf5721fe8d6055c691215bcc461c2d
meilinlu86@gmail.com <ml473@afsconnect1.njit.edu>
2016-10-26 17:55:22 -0400

    string practice 6-10

--- stringPractice.php
+++ stringPractice.php
@@ -27,5 +27,67 @@
 <?php
   echo "\n".'#5 echo '."\n";
   echo "Sum: ", 1+2;
-  echo "Hello", 
+  echo "Hello"; 
 ?>
+<?php
+  echo "\n".'#6 explode()'."\n";
+  $car = "Honda, Toyota, BMW";
+  echo $car."\n";
+  $brand = explode(", ", $car);
+  echo $brand[0];
+?>
+<?php
+  echo "\n".'#7 htmlentities'."\n";
+  $orig = "I'll \"walk\" the <b>dog</b> now";
+  $a = htmlentities($orig);
+  $b = html_entity_decode($a);
+  echo $a."\n";
+  echo $b."\n";
+?>
+<?php
+  echo "\n".'#8 htmlspecialchars_decode()'."\n";
+  $str = "<p>this -&gt; &quot;</p>\n";
+  echo htmlspecialchars_decode($str);
+  echo htmlspecialchars_decode($str, ENT_NOQUOTES);
+?>
+<?php
+  echo "\n".'#9 implode'."\n";
+  $array = array('name', 'age', 'grade');
+  $comma_separated = implode(",", $array);
+  echo $comma_separated."\n";
+  var_dump(implode('hello', array()));
+?>
+<?php
+  echo "\n".'#10 md5'."\n";
+  $str = 'apple';
+  if(md5($str)==='1f3870be274f6c49b3e31a0c6728957f'){
+    echo "Would you like a green or red apple?";
+  }
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
