db3b3249f572327bcaf71e7d7676229e1c5567ae
meilinlu86@gmail.com <ml473@afsconnect1.njit.edu>
2016-10-30 09:11:39 -0400

    if statements

---
+++ switch_isset_null_empty.php
@@ -0,0 +1,56 @@
+<?php
+$nospace='';
+if(empty($nospace)){
+  echo '$nospace is either 0, empty, or not set at all'."\n";
+}
+if(isset($nospace)){
+  echo '$nospace is set even though it is empty'."\n";
+}
+if(is_null($nospace)){
+  echo '$nospace is not NULL'."\n";
+}
+$space=' ';
+if(empty($space)){
+  echo '$space is not empty'."\n";
+}
+if(isset($space)){
+  echo '$space is set'."\n";
+}
+if(is_null($space)){
+  echo '$space is not NULL'."\n";
+}
+$nothing=NULL;
+if(empty($nothing)){
+  echo '$nothing is empty'."\n";
+}
+if(isset($nothing)){
+  echo '$nothing is not set'."\n";
+}
+if(is_null($nothing)){
+  echo '$nothing is not NULL'."\n";
+}
+$var='1';
+if(empty($var)){
+  echo '$var is not empty'."\n";
+}
+if(isset($var)){
+  echo '$var is not set'."\n";
+}
+if(is_null($var)){
+  echo '$var is not NULL'."\n";
+}
+
+echo "switch structure \n";
+
+switch($nospace){
+  case "empty($nospace)": 
+    echo "$nospace is either 0, empty, or not set at all.\n";
+  case "isset($nospace)":
+    echo "$nospace is set even though it is empty.\n";
+  case "is_null($nospace)":
+    echo "$nospace is not NULL. \n";  
+}
+
+?>
+
+
