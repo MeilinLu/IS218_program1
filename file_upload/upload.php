f30f300273200018993b7e3f08e8d4e863144c2f
meilinlu86@gmail.com <ml473@afsconnect1.njit.edu>
2016-11-20 16:16:11 -0500

    upload file

---
+++ file_upload/upload.php
@@ -0,0 +1,33 @@
+<?php
+
+define("UPLOAD_DIR","/afs/cad.njit.edu/u/m/l/ml473/public_html/UPLOADS");
+
+if(!empty($_FILES["myFile"])){
+  $myFile = $_FILES["myFile"];
+  
+  if($myFile["error"]!==UPLOAD_ERR_OK){
+    echo "ERROR!";
+    exit;
+  }
+
+  $name = preg_replace("/[^A-Z0-9._-]/i", "_", $myFile["name"]);
+  $i = 0;
+  $parts = pathinfo($name);
+  while(file_exists(UPLOAD_DIR.$NAME)){
+    $i++;
+    $name=$parts["filename"] . "-" . $i . "." . $parts["extension"];
+  }
+
+  $success = move_uploaded_file($myFile["tmp_name"], UPLOAD_DIR.$name);
+  if(!$success){
+    echo "Unable to save file <br>";
+    print_r($_FILES);
+    exit;
+  }
+  chmod(UPLOAD_DIR . $name, 0644);
+  echo $name . " is uploaded! <br>";
+}
+
+
+
+?>
